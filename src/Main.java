5be8ac7c35a621e904ae92e41c937ea699c017a0
sam06tanb <samirtajra53@gmail.com>
2023-11-06 21:02:00 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 20;
+
+        Integer obj = x;
+
+        System.out.println(obj);
+
+        int y = obj * 2;
+
+        System.out.println(y);
+
+        }
+    }
